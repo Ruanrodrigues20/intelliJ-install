37810c918db2d2492b517f9a2671597a534fb8a6
Ruanrodrigues20 <ruan.rodrigues.silvaa@gmail.com>
2026-01-21 21:44:02 -0300

    refactor: install intellij and pycharm

--- src/apps/intellij/install.sh
+++ src/apps/intellij/install.sh
@@ -10,9 +10,15 @@ export INSTALL_DIR="/opt/intellij-idea"
 export BIN_NAME="idea"
 export ICON_NAME="intellij-idea"
 export STARTUP_WMCLASS="jetbrains-idea"
-export DOWNLOAD_URL="$(python src/utils/py/get_latest_version.py IIU)"
+export DOWNLOAD_URL="$(python3 src/utils/py/get_latest_version.py IIU)"
 
 # ===============================
 # Run generic installer
 # ===============================
 bash "src/lib/install_common.sh"
+
+sudo cp /opt/pycharm/bin/pycharm.svg \
+  /usr/share/icons/hicolor/scalable/apps/pycharm.svg
+
+sudo gtk-update-icon-cache -f /usr/share/icons/hicolor
+
--- src/apps/pycharm/install.sh
+++ src/apps/pycharm/install.sh
@@ -8,9 +8,9 @@ export APP_NAME="PyCharm Professional"
 export APP_ID="pycharm"
 export INSTALL_DIR="/opt/pycharm"
 export BIN_NAME="pycharm-professional.sh"
-export ICON_NAME="pycharm-professional"
+export ICON_NAME="pycharm"
 export STARTUP_WMCLASS="pycharm-professional"
-export DOWNLOAD_URL="$(python src/utils/py/get_latest_version.py PCP)"
+export DOWNLOAD_URL="$(python3 src/utils/py/get_latest_version.py PCP)"
 
 # ===============================
 # Run generic installer
